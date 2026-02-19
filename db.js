2c2300a8b6174a97aca97ca165bb7a6ae8186498
leandroba08-art <leandroba08@gmail.com>
2026-02-19 11:22:58 -0300

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 5432),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD || ""),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
+
